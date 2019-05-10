9c2e9bb07a679362fade0fd17ee91c37cd9bff94
lpx <lpx9406@163.com>
2019-05-11 00:49:50 +0800

    Added the comment and some functions

--- MCTS_2048/main.sh
+++ MCTS_2048/main.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Obtain the date and copy the constants.py
 datestr=$(date +%Y%m%d%H%M)
 mkdir -p ./test_output/$datestr
 cp ./constants.py ./test_output/$datestr/
+
+# Write the comment file.
 echo -n "Please input the comment:"
 read comment
 echo $comment > ./test_output/$datestr/comment.txt
+
+# Run the program and write the log file 
 python -u ./main.py |tee ./test_output/$datestr/result.log
+
+# Write the summary file
 echo -n "It'over! Is there thoughts?->"
 read thoughts
 echo $thoughts > ./test_output/$datestr/summary.txt
+
+# Rename the result.log 
+echo -n "Rename the result:"
+read result_name
+mv ./test_output/$datestr/result.log ./test_output/$datestr/$result_name.log
--- train_2048/main.sh
+++ train_2048/main.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+# Obtain the date and copy the constants.py and the best_model.h5
 datestr=$(date +%Y%m%d%H%M)
 mkdir -p ./test_output/$datestr
 cp ./constants.py ./test_output/$datestr/
+cp ./Model_Resnet/ .test_output/$datestr/
+
+# Write the comment file.
 echo -n "Please input the comment:"
 read comment
 echo $comment > ./test_output/$datestr/comment.txt
+
+# Run the program and write the log file 
 python -u ./main.py |tee ./test_output/$datestr/result.log
+
+# Write the summary file
 echo -n "It'over! Is there thoughts?->"
 read thoughts
 echo $thoughts > ./test_output/$datestr/summary.txt
+
+# Rename the result.log 
+echo -n "Rename the result:"
+read result_name
+mv ./test_output/$datestr/result.log ./test_output/$datestr/$result_name.log
